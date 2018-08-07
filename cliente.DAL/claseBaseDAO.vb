c3aac0f2b0de6250dacd182e77e4c0b823d79590
JefferLindao <jefferlindao@gmail.com>
2018-08-07 10:32:26 -0500

    coneccion a la base de datos

---
+++ cliente.DAL/claseBaseDAO.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Data.SqlClient
+
+Public MustInherit Class claseBaseDAO
+    Protected Function ObtenerConexion() As SqlConnection
+        Dim oCon As New SqlConnection
+        oCon.ConnectionString = "Data Source=.\SQLDEVELOPER;Initial Catalog=db_ventas2018;Integrated Security=True"
+        Return oCon
+    End Function
+End Class
